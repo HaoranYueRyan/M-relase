6982f18b9e01fd1cb4216fe77ccda0b81723203f
HaoranYueRyan <92607722+HaoranYueRyan@users.noreply.github.com>
2023-02-13 09:08:19 +0000

    training the CNN model

--- MI_Classification/Training_GUI/main.py
+++ MI_Classification/Training_GUI/main.py
@@ -2,7 +2,7 @@ from omero_images import OmeroImages
 from training_gui import TrainingScreen, count
 import matplotlib.pyplot as plt
 
-plate_id = 9674
+plate_id = 9661
 nuclei_per_img = 100
 
 imgs = OmeroImages(plate_id, nuclei_per_img)
